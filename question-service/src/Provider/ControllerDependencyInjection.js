07d713a84a40fe2d81cf2bb90b3e43025ee86eeb
Heclis <vheeclis@gmail.com>
2019-03-30 18:13:27 -0300

    Removido serviço REST que não será usado e script desnecessário

--- question-service/src/Provider/ControllerDependencyInjection.js
+++
@@ -1,8 +0,0 @@
-const QuestionControllerFactory = require('../Controller/QuestionController');
-const { QuestionService } = require('./ProvidersDepencyInjection');
-
-const QuestionController = QuestionControllerFactory(QuestionService);
-
-module.exports = {
-  QuestionController,
-};
--- serve_environment.sh
+++ serve_environment.sh
@@ -1,6 +1,5 @@
 # !/bin/bash
 
 docker-compose -f config/dockerConfig.yml up -d
-docker exec config_mongo-database_1 bash -c "mongo < scripts/initializeMongo.js"
 
 docker-compose -f config/dockerConfig.yml logs -f
